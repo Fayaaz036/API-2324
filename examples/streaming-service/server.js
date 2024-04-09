7afa2f645d3ccdd885b7bffc36115aa71da27fb1
1190190 <fayaaz.nabie@hva.nl>
2024-04-09 13:48:51 +0200

    werkend gekregen nu bouwen

--- examples/streaming-service/server.js
+++ examples/streaming-service/server.js
@@ -20,14 +20,27 @@ app
 
 app.get('/', async (req, res) => {
   try {
-    const movieData = await fetch(`https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${process.env.MOVIEDB_TOKEN}`)
-      .then(res => res.json());
-    res.send(renderTemplate('views/index.liquid', { title: 'Movies', movieData }));
+    const trendingmovieData = await fetch(`https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${process.env.MOVIEDB_TOKEN}`)
+  .then(res => res.json());
+    res.send(renderTemplate('views/index.liquid', { title: 'Movies', trendingmovieData }));
+    console.log(trendingmovieData);
   } catch (error) {
     console.error(error);
     res.status(500).send('Error fetching movie data');
   }
 });
+app.get('/catagory', async (req, res) => {
+  try {
+    const catagoryMovies = await fetch( `https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=${process.env.MOVIEDB_TOKEN}`)
+      .then(res => res.json());
+    res.send(renderTemplate('views/index.liquid', { title: 'Movies', catagoryMovies }));
+    console.log(catagoryMovies);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching movie data');
+  }
+});
+
 
 app.get('/movie/:id/', async (req, res) => {
   try {
